dbbd473fc4fab856b1c0c1633d789646c84748c9
Ed-Mukundane <aedgar990@gmail.com>
2021-09-14 23:51:31 +0300

    buymore v1.0.0

---
+++ frontend/src/utils.js
@@ -0,0 +1,43 @@
+export const prices = [
+  {
+    name: 'Any',
+    min: 0,
+    max: 0,
+  },
+  {
+    name: `UGX:1000 to UGX:20000`,
+    min: 1,
+    max: 19999,
+  },
+  {
+    name: `UGX:20000 to UGX:50000`,
+    min: 20000,
+    max: 49999,
+  },
+  {
+    name: `UGX:50000 and Above`,
+    min: 50000,
+    max: 1000000000,
+  },
+];
+export const ratings = [
+  {
+    name: '4stars & up',
+    rating: 4,
+  },
+
+  {
+    name: '3stars & up',
+    rating: 3,
+  },
+
+  {
+    name: '2stars & up',
+    rating: 2,
+  },
+
+  {
+    name: '1stars & up',
+    rating: 1,
+  },
+];
